bdd908ee43414e5107003fcc9767ec4d5e9ad57b
Melissa <melissa160@gmail.com>
2018-03-04 18:01:35 -0500

    random name

--- app/javascript/components/home/index.tsx
+++ app/javascript/components/home/index.tsx
@@ -5,21 +5,34 @@ import PropTypes from "prop-types"
 export interface HomeProps { names: number[]; }
 
 class Index extends React.Component<HomeProps, any>{
+  constructor(props) {
+    super(props)
+    this.state = {
+      name_index: 0
+    }
+    this.setRandomIndex = this.setRandomIndex.bind(this)
+  }
+  setRandomIndex(){
+    this.setState({
+      name_index: getRandomInt(0, 3)
+    })
+  }
   render() {
     return (
       <div>
         <p>
-          {this.props.names[0]}
+          {this.props.names[this.state.name_index]}
         </p>
-        <h1>
-          {this.props.names[1]}
-        </h1>
-        <button>Seleccionador</button>
+        <button onClick={this.setRandomIndex}>Seleccionador</button>
       </div>
    )
   }
 }
  export default Index
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function formatName(person: User){
   return "Hello, " + person.firstName + " " + person.lastName;
